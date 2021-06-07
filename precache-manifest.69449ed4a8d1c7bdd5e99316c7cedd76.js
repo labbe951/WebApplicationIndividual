fe15d0fcc7bdde8853add8aa76a87f5a0813d8e6
labbe951 <haglund.pontus@hotmail.com>
2021-06-07 11:37:43 +0200

    Updates

---
+++ precache-manifest.69449ed4a8d1c7bdd5e99316c7cedd76.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3c7c4b90620f02cb55ba917f9d6bab",
+    "url": "/WebApplicationIndividual/index.html"
+  },
+  {
+    "revision": "93ad14c80214e0716ff3",
+    "url": "/WebApplicationIndividual/static/css/2.56e33495.chunk.css"
+  },
+  {
+    "revision": "3c37882ffe04897d8dc8",
+    "url": "/WebApplicationIndividual/static/css/main.95ac39e5.chunk.css"
+  },
+  {
+    "revision": "93ad14c80214e0716ff3",
+    "url": "/WebApplicationIndividual/static/js/2.c0eed67d.chunk.js"
+  },
+  {
+    "revision": "3a909cfba54d535123af21a4875dd023",
+    "url": "/WebApplicationIndividual/static/js/2.c0eed67d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c37882ffe04897d8dc8",
+    "url": "/WebApplicationIndividual/static/js/main.054a3d3d.chunk.js"
+  },
+  {
+    "revision": "98911352879b869166b1",
+    "url": "/WebApplicationIndividual/static/js/runtime-main.9d3018e8.js"
+  },
+  {
+    "revision": "2e83439664c8f58124fbb7d3f44f5d33",
+    "url": "/WebApplicationIndividual/static/media/globalsecurity.2e834396.png"
+  }
+]);
